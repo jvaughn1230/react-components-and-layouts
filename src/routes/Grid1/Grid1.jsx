1b0d8bac9d13cd050190d6c8e2e93faa8ed0517c
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-06-24 14:54:24 -0700

    adding Grid1

--- src/routes/Grid1/Grid1.jsx
+++ src/routes/Grid1/Grid1.jsx
@@ -7,6 +7,7 @@ function Grid1() {
     <div className='container'>
       <div className='col1 col'>
         <h1>COL 1</h1>
+        <p>Lorem ipsum dolor sit amet. Est dolores veritatis qui sunt doloribus hic corrupti quae et odit consequatur non error debitis est voluptatem perspiciatis. Sit quibusdam galisum in ullam dolores ut velit quia sed eveniet autem et dolore nobis sit rerum possimus rem corrupti cupiditate.Ut tempore cupiditate eos beatae sunt qui facilis ullam non officiis praesentium est error voluptatem? Est corrupti placeat quo error tempora ut inventore totam non inventore ducimus.Qui quisquam saepe sit voluptas voluptas ea expedita architecto ea quia rerum et quia culpa qui laboriosam explicabo eum voluptatem veniam. Et aliquid voluptates sit laudantium fugit vel odio consequatur sed ratione accusamus eos quibusdam quidem aut pariatur assumenda. Vel saepe consequatur id voluptas quasi vel rerum veniam aut quia repudiandae ut illum neque et officia tenetur. Et doloribus expedita est vero aperiam et voluptatem autem qui officia numquam non repudiandae totam qui error quia qui eaque voluptatibus.</p>
       </div>
       <div className='col2 col'>
         <h1>COL 2</h1>
